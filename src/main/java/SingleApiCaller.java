5d67d1b7fdc00a930e4601e2b18cea0e68f571fc
Raffaele Ragni <raffaele.ragni@deliveryhero.com>
2020-11-18 14:33:52 +0100

    http client and wiremock

--- src/main/java/SingleApiCaller.java
+++ src/main/java/SingleApiCaller.java
@@ -1,13 +1,30 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Objects;
 
 public class SingleApiCaller implements ApiCaller {
 
+  final HttpClient client;
+
+  public SingleApiCaller(HttpClient client) {
+    this.client = client;
+  }
+
   @Override
   public String call(String endpointUrl) {
     Objects.requireNonNull(endpointUrl, "Null URL is passed");
-    
-    return null;
+
+    var request = HttpRequest.newBuilder()
+      .uri(URI.create(endpointUrl))
+      .build();
+
+    var futureResponse = client
+      .sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+    return futureResponse.join().body();
   }
 
-  
+
 }
--- src/test/java/SingleApiCallerTest.java
+++ src/test/java/SingleApiCallerTest.java
@@ -1,23 +1,50 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.concurrent.CompletableFuture;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SingleApiCallerTest {
 
   SingleApiCaller apiCaller;
-  
+  HttpClient client;
+
   @BeforeEach
   void setup() {
-    apiCaller = new SingleApiCaller();
+    client = mock(HttpClient.class);
+    apiCaller = new SingleApiCaller(client);
   }
-  
+
   @Test
   void testCallWithNullUrl() {
     var ex = assertThrows(NullPointerException.class, () -> apiCaller.call(null));
     assertThat(ex.getMessage(), is("Null URL is passed"));
+  }
+
+  @Test
+  void testHttpClient() {
+    var request = HttpRequest.newBuilder()
+      .uri(URI.create("http://openjdk.java.net/"))
+      .build();
+    var response = mock(HttpResponse.class);
+    when(client.sendAsync(request, BodyHandlers.ofString()))
+      .thenReturn(CompletableFuture.completedFuture(response));
+
+    when(response.body()).thenReturn("response1");
+
+    var result = apiCaller.call("http://openjdk.java.net/");
+
+    assertThat(result, is("response1"));
   }
 }
